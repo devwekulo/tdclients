969df18892761906b46e6e74c3b354614a311820
Kevin WEkulo <wekulo@outlook.com>
2021-12-04 11:36:26 +0300

    ORM Setup and Entity Generation

---
+++ src/Entity/Client.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClientRepository::class)
+ */
+class Client
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $passport;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $issued;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $expiry;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nationality;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $organisation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getPassport(): ?string
+    {
+        return $this->passport;
+    }
+
+    public function setPassport(string $passport): self
+    {
+        $this->passport = $passport;
+
+        return $this;
+    }
+
+    public function getIssued(): ?\DateTimeInterface
+    {
+        return $this->issued;
+    }
+
+    public function setIssued(\DateTimeInterface $issued): self
+    {
+        $this->issued = $issued;
+
+        return $this;
+    }
+
+    public function getExpiry(): ?\DateTimeInterface
+    {
+        return $this->expiry;
+    }
+
+    public function setExpiry(\DateTimeInterface $expiry): self
+    {
+        $this->expiry = $expiry;
+
+        return $this;
+    }
+
+    public function getNationality(): ?string
+    {
+        return $this->nationality;
+    }
+
+    public function setNationality(string $nationality): self
+    {
+        $this->nationality = $nationality;
+
+        return $this;
+    }
+
+    public function getOrganisation(): ?string
+    {
+        return $this->organisation;
+    }
+
+    public function setOrganisation(?string $organisation): self
+    {
+        $this->organisation = $organisation;
+
+        return $this;
+    }
+}
